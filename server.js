eb1eaaa4e0f239598cb00efcb62e80cd8239e657
spidergroupcm <spidergroupcm@gmail.com>
2026-04-30 15:03:50 +0600

    Fixed some Bugs

--- server.js
+++ server.js
@@ -26,4 +26,4 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/eventora')
   .catch(err => console.error('MongoDB Connection Error:', err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
